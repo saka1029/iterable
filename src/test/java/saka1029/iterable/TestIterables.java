39fb7f7b51b67755c3dab8aa61dedc284320a214
saka1029 <saka1029@gmail.com>
2024-12-28 18:46:22 +0900

    Permutation, Combination カバレッジ100%

--- src/test/java/saka1029/iterable/TestIterables.java
+++ src/test/java/saka1029/iterable/TestIterables.java
@@ -1,8 +1,10 @@
 package saka1029.iterable;
 
 import static saka1029.iterable.Iterables.*;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
@@ -14,6 +16,29 @@ public class TestIterables {
         assertEquals(List.of(), list());
         assertEquals(List.of(0, 1, 2), list(0, 1, 2));
         assertEquals(List.of("a", "b"), list("a", "b"));
+    }
+
+    @Test
+    public void testArrayList() {
+        ArrayList<Integer> list = arrayList(0, 1, 2);
+        assertEquals(List.of(0, 1, 2), list);
+    }
+
+    @Test
+    public void testLinkedList() {
+        LinkedList<Integer> list = linkedList(0, 1, 2);
+        assertEquals(List.of(0, 1, 2), list);
+    }
+
+    @Test
+    public void testLinkedListFromIterable() {
+        LinkedList<Integer> list = linkedList(list(0, 1, 2));
+        assertEquals(List.of(0, 1, 2), list);
+    }
+
+    @Test
+    public void testArray() {
+        assertArrayEquals(new String[] {"a", "b", "c"}, array(String[]::new, list("a", "b", "c")));
     }
 
     @Test
