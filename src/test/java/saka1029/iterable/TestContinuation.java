fba0e823f9a4d40c569e8abe83cc2742124dc364
saka1029 <saka1029@gmail.com>
2025-01-14 21:06:09 +0900

    Update TestContinuation

--- src/test/java/saka1029/iterable/TestContinuation.java
+++ src/test/java/saka1029/iterable/TestContinuation.java
@@ -1,58 +1,116 @@
 package saka1029.iterable;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.HashMap;
-import java.util.Map;
-
+import java.io.Closeable;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.logging.Logger;
 import org.junit.Test;
+import saka1029.Common;
 
 public class TestContinuation {
 
-    enum ResultType {
-        CONTINUE,
-        YIELD,
-        BREAK,
-        END;
+    static final Logger logger = Common.logger(TestContinuation.class);
+
+    interface CoroutineBody<T> {
+        void accept(Coroutine<T> coroutine) throws InterruptedException;
     }
 
-    record Result(ResultType type, int value) {}
-    static final Result CONTINUE = new Result(ResultType.CONTINUE, -1);
-    static final Result BREAK = new Result(ResultType.CONTINUE, -1);
-    static final Result END = new Result(ResultType.END, -1);
-    static Result yields(int value) { return new Result(ResultType.YIELD, value); }
-
-    static class Context {
-        int pc = 0;
-        Map<String, Integer> vars = new HashMap<>();
-    }
-    
-    interface Statement {
-        Result run(Context c);
+    interface CoroutineRun {
+        void run() throws InterruptedException;
     }
 
-    static Statement sequential(Statement... statements) {
-        return c -> {
-            while (c.pc < statements.length) {
-                Result r = statements[c.pc++].run(c);
-                if (r.type == ResultType.YIELD)
-                    return r;
-            }
-            return END;
-        };
+    static class Coroutine<T> implements Closeable {
+
+        final int size = 4;
+        final Queue<T> que = new LinkedList<>();
+        Thread thread;
+
+        public void start(CoroutineRun body) {
+            que.clear();
+            Runnable runnable = () -> {
+                try {
+                    body.run();
+                } catch (InterruptedException e) {
+                    logger.info("thread end");
+                }
+            };
+            thread = new Thread(runnable);
+            thread.start();
+        }
+
+        public void start(CoroutineBody<T> body) {
+            que.clear();
+            Runnable runnable = () -> {
+                try {
+                    body.accept(this);
+                } catch (InterruptedException e) {
+                    logger.info("thread end");
+                }
+            };
+            thread = new Thread(runnable);
+            thread.start();
+        }
+
+        public synchronized void yield(T newValue) throws InterruptedException {
+            if (Thread.currentThread().isInterrupted())
+                throw new InterruptedException();
+            while (que.size() >= size)
+                wait();
+            que.add(newValue);
+            notify();
+        }
+
+        public synchronized T take() {
+            if (!thread.isAlive() && que.size() <= 0)
+                throw new NoSuchElementException("No yield element");
+            while (que.size() <= 0)
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            T result = que.remove();
+            notify();
+            return result;
+        }
+
+        @Override
+        public void close() {
+            thread.interrupt();
+        }
     }
 
     @Test
-    public void testStatement() {
-        Statement statements = sequential(
-            c -> { c.vars.put("a", 100); return CONTINUE; },
-            c -> { c.vars.put("b", c.vars.get("a") + 10); return CONTINUE; },
-            c -> yields(c.vars.get("b")),
-            c -> yields(999)
-        );
-        Context context = new Context();
-        assertEquals(yields(110), statements.run(context));
-        assertEquals(yields(999), statements.run(context));
-        assertEquals(END, statements.run(context));
+    public void testCoroutine() {
+        try (var coroutine = new Coroutine<>()) {
+            coroutine.start(c -> {
+                for (int i = 0; true; ++i)
+                    c.yield(i);
+            });
+            logger.info("take: " + coroutine.take());
+        }
+    }
+
+    @Test
+    public void testCoroutineRun() {
+        try (var coroutine = new Coroutine<>()) {
+            coroutine.start(() -> {
+                for (int i = 0; true; ++i)
+                    coroutine.yield(i);
+            });
+            logger.info("take: " + coroutine.take());
+        }
+    }
+
+    @Test
+    public void testTakeTooMatch() {
+        try (var coroutine = new Coroutine<>()) {
+            coroutine.start(() -> {
+                coroutine.yield(3);
+            });
+            logger.info("take: " + coroutine.take());
+            logger.info("take: " + coroutine.take());
+        }
     }
 }
